d8823a61b89fb087b575116d908f9dbc32ca9108
Derek Erwin <none>
2022-08-27 20:48:40 -0400

    Added all the javascript logic to play in the console.

--- javascript.js
+++ javascript.js
@@ -1,23 +1,111 @@
-console.log("Hello, World!")
-    
-function favoriteAnimal(animal) {
-  console.log(animal + " is my favorite animal!")
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-favoriteAnimal('Goat')
-
-function addTwo(numOne = 5, numTwo = 1) {
-  console.warn(numOne + numTwo);
+function getComputerChoice() {
+  let choice = getRandomInt(3);
+  switch (choice) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+      break;
+  }
 }
 
-function myFunction() {
-  let num = 3;
-  return num;
+function getPlayerChoice(currentRound = 1, rounds = 1){
+  let choice = "";
+  while (choice == "") {
+    choice = prompt(`${currentRound + 1} of ${rounds} - Choose rock, paper or scissors.`).toLowerCase();
+    switch (choice) {
+      case "rock":
+        return "rock";
+        break;
+      case "paper":
+        return "paper";
+        break;
+      case "scissors":
+        return "scissors";
+        break;
+      default:
+        choice = "";
+        console.warn("Not a correct choice.")
+    }
+  }
 }
 
-console.log(myFunction());
-// calls the function once
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice == computerChoice){
+    console.warn(`Tie! ${playerChoice} vs ${computerChoice}.`);
+    return "tie";
+  }
 
-addTwo();
-addTwo(4);
-addTwo(2, 7);
+  switch (playerChoice) {
+    case "rock":
+      switch (computerChoice){
+        case "paper":
+          console.log(`You win! ${playerChoice} vs ${computerChoice}.`);
+          return "win";
+          break;
+        case "scissors":
+          console.error(`You lose! ${playerChoice} vs ${computerChoice}.`);
+          return "lose";
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerChoice){
+        case "rock":
+          console.log(`You win! ${playerChoice} vs ${computerChoice}.`);
+          return "win";
+          break;
+        case "scissors":
+          console.error(`You lose! ${playerChoice} vs ${computerChoice}.`);
+          return "lose";
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerChoice){
+        case "paper":
+          console.log(`You win! ${playerChoice} vs ${computerChoice}.`);
+          return "win";
+          break;
+        case "rock":
+          console.error(`You lose! ${playerChoice} vs ${computerChoice}.`);
+          return "lose";
+          break;
+      }
+      break;
+  }
+}
+
+function game(rounds = 1) {
+  let wins = 0;
+  let losses = 0;
+  let ties = 0;
+  for (let i = 0; i < rounds; i++) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice(i, rounds);
+    let outcome = playRound(playerChoice, computerChoice);
+
+    switch (outcome) {
+      case "win":
+        ++wins;
+        break;
+      case "lose":
+        ++losses;
+        break;
+      case "tie":
+        ++ties;
+        break;
+    }
+  }
+  console.log(`Finished with ${wins} wins, ${losses} losses, and ${ties} ties.`);
+}
+
+game(3);
